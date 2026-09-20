0aa86f5ba5ed9edff84f991aaf1e3903bd83b49c
asivery <asivery@protonmail.com>
2026-09-20 09:49:05 +0200

    Cleanup linter issues

--- ffi/input_qtfb.lua
+++ ffi/input_qtfb.lua
@@ -153,20 +153,18 @@ local function translate_input(msg)
             emit(C.EV_ABS, C.ABS_PRESSURE, dTranslate)
             emit(C.EV_SYN, C.SYN_REPORT, 0)
         elseif inputType == 0x30 then -- INPUT_BTN_PRESS
-            local key = 0
+            local key = x
             if x == 0 then key = 105 -- KEY_LEFT
             elseif x == 1 then key = 102 -- KEY_HOME
             elseif x == 2 then key = 106 -- KEY_RIGHT
-            else key = x
             end
             emit(C.EV_KEY, key, 1)
             emit(C.EV_SYN, C.SYN_REPORT, 0)
         elseif inputType == 0x31 then -- INPUT_BTN_RELEASE
-            local key = 0
+            local key = x
             if x == 0 then key = 105
             elseif x == 1 then key = 102
             elseif x == 2 then key = 106
-            else key = x
             end
             emit(C.EV_KEY, key, 0)
             emit(C.EV_SYN, C.SYN_REPORT, 0)
